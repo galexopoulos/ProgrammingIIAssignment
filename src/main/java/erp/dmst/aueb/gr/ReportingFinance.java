27480a1efb9431bd1138718c9016dd37d1901113
alexisperakis <alexisperakis@alexiss-mbp.lan>
2019-11-12 19:25:01 +0200

    Add constructor and some methods change instant variables

--- src/main/java/erp/dmst/aueb/gr/ReportingFinance.java
+++ src/main/java/erp/dmst/aueb/gr/ReportingFinance.java
@@ -1,15 +1,24 @@
 package main.java.erp.dmst.aueb.gr;
 import java.util.Scanner;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class ReportingFinance {
 	
-		private final double electricity = 4000; //logariasmos DEH
-		private final double waterSupply = 4500; //logarismos EYDAP
-		private final double phone_internetSupply = 650; //logariasmos tilefonou kai internet
+		private double electricity; //logariasmos DEH
+		private double waterSupply ; //logarismos EYDAP
+		private double phone_internetSupply; //logariasmos tilefonou kai internet
 		
+		public ReportingFinance(double electricity, double waterSupply, double phone_internetSupply) {
+			super();
+			this.electricity = electricity;
+			this.waterSupply = waterSupply;
+			this.phone_internetSupply = phone_internetSupply;
+		}
+
 		public static void getMenu() {
 			Scanner sc = new Scanner(System.in);
-			System.out.println("--MENU FINANCE--"
+			System.out.println("--MENU FINANCE--" + getDate()
 					+ "/n 1. Proceeds - Εxpenses - Profits - Losses"
 					+ "/n 2. Loans "
 					+ "/n 3. TAX liabilities ");
@@ -21,8 +30,8 @@ public class ReportingFinance {
 					switch(ans) {
 						case 1:
 					}
-				confirm = false;
 					
+				confirm = false;
 				}catch(Exception e) {
 					System.err.println("Wrong input, please try again.");
 				}
@@ -30,16 +39,47 @@ public class ReportingFinance {
 			
 		}
 		
-		public double getProceeds() {
-			// Booking.getchecks
+		public static double getProceeds() {
+			if(Booking.getChecks > 0) {
+				return Booking.getChecks;
+			}else return 0;
 		}
 		
-		public double getExpenses() {
-			//
+		public double getElectricity() {
+			return electricity;
+		}
+
+		public void setElectricity(double electricity) {
+			this.electricity = electricity;
+		}
+
+		public double getWaterSupply() {
+			return waterSupply;
+		}
+
+		public void setWaterSupply(double waterSupply) {
+			this.waterSupply = waterSupply;
+		}
+
+		public double getPhone_internetSupply() {
+			return phone_internetSupply;
+		}
+
+		public void setPhone_internetSupply(double phone_internetSupply) {
+			this.phone_internetSupply = phone_internetSupply;
 		}
 		
 		public double getProfit_losses() {
-			//
+			double sumOfFixesExpenses = waterSupply + phone_internetSupply + electricity;
+			//Pending HR wages.
+			double sumOfIncome = getProceeds(); // + George's Markou Buffet expenses - pending 
+			return sumOfFixesExpenses - sumOfIncome;
+		}
+		
+		public static String getDate() {
+			SimpleDateFormat formatter= new SimpleDateFormat("dd-MM-yyyy, HH:mm:ss");
+			Date date = new Date(System.currentTimeMillis());
+			return formatter.format(date);
 		}
 
 }
