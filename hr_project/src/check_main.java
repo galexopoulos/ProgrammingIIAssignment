06839b268f3d2d7ff48b05b52b2b6a4d3d37c8af
Νίκος <@192.168.1.7>
2019-12-20 22:47:15 +0200

    added mail on hr director

--- hr_project/src/check_main.java
+++ hr_project/src/check_main.java
@@ -32,7 +32,7 @@ public class check_main {
 /*2*/	Manager Barney = new Manager("Barney", "Stinson", "P.L.E.A.S.E.", "robin", 1434133, null);
 /*3*/	Employee Ivan = new Employee("Ivan", "Zaytsev", "Athlete", "Petreasismydaddy", 12000, Petreas);
 /*4*/	Employee Rick = new Employee("Rick", "Sanchez", "scientist", "Wubalubadubdub", 2, Barney);
-/*5*/	Employee Rachel = new Employee("Rachel", "Green", "waitress", "l", 980, Barney);
+/*5*/	Employee Rachel = new Employee("Rachel", "Green", "waitress", "LaPooh", 980, Barney);
 /*6*/	Employee NF = new Employee("Nate", "Feuerstein", "GOAT", "paidmydues", 3034308, Barney);
 		String[] shift = { "11:00-17:00", "10:00-18:30", "-", "7:00-14:00", "22:00-", "-7:00,12:00-13:00", "-",
 				"11:00-17:00" };
