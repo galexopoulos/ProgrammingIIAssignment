a69c41c802a4f4782c7dc9615fa4864345c6d0c5
mant6 <mant6@LAPTOP-92U1E0SS>
2019-12-23 15:30:49 +0200

    junits

--- hr_project/src/AllTests.java
+++
@@ -1,9 +0,0 @@
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-import org.junit.runners.Suite.SuiteClasses;
-
-@RunWith(Suite.class)
-@SuiteClasses({checkDecimalsSalary_Junit.class, binary_search_unit.class})
-public class AllTests {
-
-}
