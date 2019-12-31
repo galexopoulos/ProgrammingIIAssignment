df2133755ac7c8fbe809e2391d616ec1f6b21119
georg <georg@LAPTOP-39JODKCQ>
2019-12-31 17:03:05 +0200

    junit related changes

--- src/empBinarySearchTest.java
+++ src/empBinarySearchTest.java
@@ -1,15 +1,16 @@
+
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-class binary_search_unit {
+class empBinarySearchTest {
 
 	@Test
 	void test() {
 		new Employee("Ivan", "Zaytsev", "Athlete", "Petreasismydaddy", 12000, null);
 		new Employee("Ivan", "Zaytsev", "Athlete", "Petreasismydaddy", 12000, null);
 
-		int output = Employee.binarySearch(1);
+		int output = Employee.empBinarySearch(1);
 		assertEquals(1, output);
 	}
 
