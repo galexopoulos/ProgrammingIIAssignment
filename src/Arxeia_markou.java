4449a557e58ea843e3aaa714f3dce81d8a1905fe
mant6 <mant6@LAPTOP-92U1E0SS>
2020-01-06 20:52:13 +0200

    Serialization Inventory

---
+++ src/Arxeia_markou.java
@@ -0,0 +1,73 @@
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+
+public class Arxeia_markou {
+	public static void grapsimo_Inventory() {
+		try {
+
+			ObjectOutputStream out = new ObjectOutputStream(
+					new FileOutputStream("C:\\Users\\mant6\\eclipse-workspace\\MARKOU\\markou.ser"));
+			out.writeObject(Inventory.fixedInventory);
+			out.close();
+			out.close();
+		} catch (Exception i) {
+			i.printStackTrace();
+		}
+		try {
+
+			ObjectOutputStream out = new ObjectOutputStream(
+					new FileOutputStream("C:\\Users\\mant6\\eclipse-workspace\\MARKOU\\markou2.ser"));
+			out.writeObject(Inventory.urgentInventory);
+			out.close();
+			out.close();
+		} catch (Exception i) {
+			i.printStackTrace();
+		}
+
+	}
+
+	public static void parsimo_Inventory() {
+		try {
+			FileInputStream fis = new FileInputStream("C:\\Users\\mant6\\eclipse-workspace\\MARKOU\\markou.ser");
+			ObjectInputStream ois = new ObjectInputStream(fis);
+			Inventory.setFixedInventory((ArrayList) ois.readObject());
+			// Inventory.setFixedInventory((ArrayList) ois.readObject());
+			ois.close();
+			fis.close();
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+			return;
+		} catch (ClassNotFoundException c) {
+			System.out.println("Class not found");
+			c.printStackTrace();
+			return;
+		}
+		try {
+			FileInputStream fis = new FileInputStream("C:\\Users\\mant6\\eclipse-workspace\\MARKOU\\markou2.ser");
+			ObjectInputStream ois = new ObjectInputStream(fis);
+			Inventory.setUrgentInventory((ArrayList) ois.readObject());
+			// Inventory.setFixedInventory((ArrayList) ois.readObject());
+			ois.close();
+			fis.close();
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+			return;
+		} catch (ClassNotFoundException c) {
+			System.out.println("Class not found");
+			c.printStackTrace();
+			return;
+		}
+	}
+
+	public static void Emfanish() {
+		for (int i = 0; i < Inventory.getFixedInventory().size(); i++) {
+			System.out.println(Inventory.getFixedInventory().get(i));
+		}
+	}
+
+}
