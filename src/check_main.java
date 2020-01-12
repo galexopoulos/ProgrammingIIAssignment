975cf446fd59820168ac4968a0ee9c54e2f3a8ab
Νίκος <@192.168.1.9>
2020-01-12 17:44:59 +0200

    loadobjects

--- src/check_main.java
+++ src/check_main.java
@@ -79,21 +79,22 @@ public class check_main {
 	}
 
 	public static void loadobjects() {
-		/* 0 */ Hr_Director Kobe = new Hr_Director("Kobe", "Bryant", "123456", 213141);
-		/* 1 */ Manager Petreas = new Manager("Petreas", "Giorgos", "Athlete", "DaddyofIvan", 19000, Kobe);
-		/* 2 */ Manager Barney = new Manager("Barney", "Stinson", "P.L.E.A.S.E.", "robin", 1434133, Kobe);
+		/* 0 */ Hr_Director Kobe = new Hr_Director("Kobe", "Bryant", "123456", 2131);
+		/* 1 */ Manager Petreas = new Manager("Petreas", "Giorgos", "Athlete", "DaddyofIvan", 1900, Kobe);
+		/* 2 */ Manager Barney = new Manager("Barney", "Stinson", "P.L.E.A.S.E.", "robin", 1434, Kobe);
 		/* 3 */ Manager Dumbledore = new Manager("Albus", "Percival Wulfric Brian Dumbledore", "Principal", "oldman",
 				2200, Kobe);
 		/* 4 */ Manager Leo = new Manager("Leonardo", "Da vinci", "Architect", "monalisa", 2100, Kobe);
-		/* 5 */ Employee Ivan = new Employee("Ivan", "Zaytsev", "Athlete", "Petreasismydaddy", 12000, Petreas);
+		/* 5 */ Employee Ivan = new Employee("Ivan", "Zaytsev", "Athlete", "Petreasismydaddy", 1200, Petreas);
 		/* 6 */ Employee Rick = new Employee("Rick", "Sanchez", "scientist", "Wubalubadubdub", 2, Barney);
 		/* 7 */ Employee Rachel = new Employee("Rachel", "Green", "waitress", "LaPooh", 980, Barney);
-		/* 8 */ Employee NF = new Employee("Nate", "Feuerstein", "GOAT", "paidmydues", 3034308, Barney);
+		/* 8 */ Employee NF = new Employee("Nate", "Feuerstein", "GOAT", "paidmydues", 2200, Barney);
 		/* 9 */ Employee Ron = new Employee("Ron", "Wesley", "Wizard", "orangehair", 1600, Barney);
 		/* 10 */ Employee Lionel = new Employee("Lionel", "Messi", "Athlete", "goat", 750, Dumbledore);
 		/* 11 */ Employee Cristiano = new Employee("Cristiano", "Ronaldo", "Athlete", "beyondmessi", 749, Dumbledore);
 		/* 12 */ Employee Margaret = new Employee("Margaret", "Thatcher", "Queen", "obey", 1250, Dumbledore);
 		/* 13 */ Employee Angela = new Employee("Angela", "Merkel", "chancellor", "ichheißemerkel", 1000, Dumbledore);
 		/* 14 */ Employee Agatha = new Employee("Agatha", "Christie", "Author", "missmarple", 700, Petreas);
+	
 	}
 }
