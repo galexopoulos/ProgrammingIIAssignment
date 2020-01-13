56dc1bb311eb60ccc304f2ffea9d41230cfc8fe5
Alexis Perakis <56519479+alexisperakis@users.noreply.github.com>
2020-01-13 02:54:26 +0200

    Delete ReportingHr
    ReportingHr has been deleted so some methods and objects of that class must be deleted from all the other classes

--- src/main/java/erp/dmst/aueb/gr/JUnitDistribution_Star.java
+++ src/main/java/erp/dmst/aueb/gr/JUnitDistribution_Star.java
@@ -45,16 +45,7 @@ public class JUnitDistribution_Star {
 	}
 
 	static  void loadobjects() {
-		ReportingHR r1 = new ReportingHR(1200,"alexis","perakis");
-		ReportingHR r2 = new ReportingHR(1200,"nikos","papadopoulos");
-		ReportingHR r3 = new ReportingHR(1200,"giorgos","moatsos");
-		ReportingHR r4 = new ReportingHR(1200,"babis","markou");
-		ReportingHR r5 = new ReportingHR(1150,"dimitris","tsoutrelis");
-		ReportingHR r6 = new ReportingHR(1150,"alexandros","sideris");
-		ReportingHR r7 = new ReportingHR(2000,"niki","pavlopoulou");
-		ReportingHR r8 = new ReportingHR(2000,"sofia","kafouni");
-		ReportingHR r9 = new ReportingHR(2100,"stefania","terzopoulou");
-		ReportingHR r10 = new ReportingHR(780,"katerina","patera");
+
 
 		ReportingFinance f = new ReportingFinance(4500,5000,500); // orizoume tous logarismous tou mina
 		ReportingFinance f1 = new ReportingFinance(4509,5232,443);
@@ -62,18 +53,21 @@ public class JUnitDistribution_Star {
 		ReportingFinance f3 = new ReportingFinance(7509,5232,443);
 		ReportingFinance f4 = new ReportingFinance(4655,4553,434);
 
-		ReportingClients c1 = new ReportingClients("alexis","perakis",5,340,0,17,19,2);
-		ReportingClients c2 = new ReportingClients("nikos","antonopoulos",3,50,2,11,12,0);
-		ReportingClients c3 = new ReportingClients("dimitris","dimitropoulos",5,540,2,7,11,2);
-		ReportingClients c4 = new ReportingClients("notis","lapatas",5,240,2,23,27,1);
-		ReportingClients c5 = new ReportingClients("christina","kardami",5,3300,2,20,29,1);
-		ReportingClients c6 = new ReportingClients("kostantina","epithetopoulou",4,300,1,15,17,1);
-		ReportingClients c7 = new ReportingClients("onomas","spetsiopoulos",4,290,0,17,19,2);
-		ReportingClients c8 = new ReportingClients("dimitris","gianakopoulos",5,470,0,17,19,0);
-		ReportingClients c9 = new ReportingClients("christina","katsaba",5,335,2,8,14,0);
-		ReportingClients c10 = new ReportingClients("takis","tsoukalas",5,535,2,8,14,0);
-		ReportingClients c11 = new ReportingClients("Vaggelis","Marinakis",5,475,2,8,14,0);
-		ReportingClients c12 = new ReportingClients("Dimitris","Giannakopoulos",5,455,2,8,14,0);
+		ReportingClients c1 = new ReportingClients("alexis", "Perakis", 5, 230, 2, 5, 1, "abc@gmail.com", 2);
+		ReportingClients c2 = new ReportingClients("Giorgos", "Alexopoulos", 5, 150, 1, 3, 2, "abc1@gmail.com", 2);
+		ReportingClients c3 = new ReportingClients("Dimitris", "gianakopoulos", 5, 210, 3, 5, 0, "dpg7000@gmail.com", 2);
+		ReportingClients c4 = new ReportingClients("Takis", "Tsoukalas", 4, 70, 1, 4, 4, "takaros@gmail.com", 2);
+		ReportingClients c5 = new ReportingClients("Vaggelis", "Marinakis", 4, 90, 2, 5, 0, "Vagelas7@gmail.com", 2);
+		ReportingClients c6 = new ReportingClients("Stefania", "Terzopoulou", 4, 85, 3, 6, 0, "stefterzo@gmail.com", 2);
+		ReportingClients c7 = new ReportingClients("Christina", "Maragou", 3, 530, 3, 2, 3, "chrismar@gmail.com", 2);
+		ReportingClients c8 = new ReportingClients("Elena", "kouts", 3, 400, 3, 2, 2, "kouts@gmail.com", 2);
+		ReportingClients c9 = new ReportingClients("Katerina", "Patera", 3, 350, 3, 3, 3, "katpat@gmail.com", 2);
+		ReportingClients c10 = new ReportingClients("Marina", "Pepelasi",3, 125, 2, 4, 3, "mar@gmail.com", 2);
+		ReportingClients c11 = new ReportingClients("Aggelina", "Kafouni",5, 100, 1, 2, 3, "aggee@gmail.com", 2);
+		ReportingClients c12 = new ReportingClients("Eleni", "Vasilopoulou",5, 550, 1, 5, 1, "elenivas@gmail.com", 2);
+		ReportingClients c13 = new ReportingClients("Katerina", "Tzima",5, 630, 2, 6, 4, "kat@gmail.com", 2);
+		ReportingClients c14 = new ReportingClients("Panagiotis", "Aggelopoulos",5, 540, 1, 3, 4, "Olympiacos7@gmail.com", 2);
+		ReportingClients c15 = new ReportingClients("Giorgos", "Aggelopoulos", 5, 130, 3, 5, 1,"hrilikosMagas7@gmail.com", 2);
 	}
 
 }
