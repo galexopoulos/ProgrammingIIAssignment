83fe42f1272275c7331388fbd28ff5c117f01176
Nikolas <Nikolas@DESKTOP-CG1EUKH>
2020-01-14 18:15:08 +0200

    trying to fix test

--- src/src/test/java/GetVisitorsTest.java
+++ src/src/test/java/GetVisitorsTest.java
@@ -1,5 +1,6 @@
 package src.test.java;
 
+import java.util.Calendar;
 import java.util.Date;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
@@ -10,13 +11,45 @@ class GetVisitorsTest {
 	void test() {
 		src.main.java.gr.aueb.dmst.erp.Room r1 = new src.main.java.gr.aueb.dmst.erp.Room(1, 2, 50);
 		src.main.java.gr.aueb.dmst.erp.Room r2 = new src.main.java.gr.aueb.dmst.erp.Room(1, 2, 50);
-		Date checkIn1 = new Date();
-		Date checkOut1 = new Date();
+		Calendar Cal = Calendar.getInstance();
+		Cal.set(Calendar.YEAR, 2020);
+		Cal.set(Calendar.MONTH, 1 - 1);
+		Cal.set(Calendar.DAY_OF_MONTH, 13);
+		Cal.set(Calendar.HOUR_OF_DAY, 12);
+		Cal.set(Calendar.MINUTE, 0);
+		Cal.set(Calendar.SECOND, 0);
+		Cal.set(Calendar.MILLISECOND, 0);
+		Date checkIn1 = Cal.getTime();
+		Cal = Calendar.getInstance();
+		Cal.set(Calendar.YEAR, 2020);
+		Cal.set(Calendar.MONTH, 1 - 1);
+		Cal.set(Calendar.DAY_OF_MONTH, 17);
+		Cal.set(Calendar.HOUR_OF_DAY, 11);
+		Cal.set(Calendar.MINUTE, 0);
+		Cal.set(Calendar.SECOND, 0);
+		Cal.set(Calendar.MILLISECOND, 0);
+		Date checkOut1 = Cal.getTime();
 		src.main.java.gr.aueb.dmst.erp.Booking b1 = new src.main.java.gr.aueb.dmst.erp.Booking(checkIn1, checkOut1, 1,
 				true);
 		b1.setCheckedIn(true);
-		Date checkIn2 = new Date();
-		Date checkOut2 = new Date();
+		Cal = Calendar.getInstance();
+		Cal.set(Calendar.YEAR, 2020);
+		Cal.set(Calendar.MONTH, 1 - 1);
+		Cal.set(Calendar.DAY_OF_MONTH, 14);
+		Cal.set(Calendar.HOUR_OF_DAY, 12);
+		Cal.set(Calendar.MINUTE, 0);
+		Cal.set(Calendar.SECOND, 0);
+		Cal.set(Calendar.MILLISECOND, 0);
+		Date checkIn2 = Cal.getTime();
+		Cal = Calendar.getInstance();
+		Cal.set(Calendar.YEAR, 2020);
+		Cal.set(Calendar.MONTH, 1 - 1);
+		Cal.set(Calendar.DAY_OF_MONTH, 18);
+		Cal.set(Calendar.HOUR_OF_DAY, 11);
+		Cal.set(Calendar.MINUTE, 0);
+		Cal.set(Calendar.SECOND, 0);
+		Cal.set(Calendar.MILLISECOND, 0);
+		Date checkOut2 = Cal.getTime();
 		src.main.java.gr.aueb.dmst.erp.Booking b2 = new src.main.java.gr.aueb.dmst.erp.Booking(checkIn2, checkOut2, 2,
 				false);
 		b2.setCheckedIn(true);
