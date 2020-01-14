23d4b78da9d42ba8543aab9519411223c5e2f814
George Alkis Alexopoulos <56980115+galexopoulos@users.noreply.github.com>
2020-01-14 22:07:53 +0200

    Final Commit (i guess)

---
+++ src/src/main/java/gr/aueb/dmst/erp/Membership.java
@@ -0,0 +1,39 @@
+
+public enum Membership {
+	/** This 'enum' class is used for the account customers in CustomerEntry class 
+	 * and defines the discount percentage for every stage of the loyalty card.
+	 * @author Georgios Alkis Alexopoulos.
+	 */
+
+	DIAMOND{
+        @Override
+        public double beneffitsPay() {
+            return 0.2;
+        }
+       
+    },
+    GOLD{
+        @Override
+        public double beneffitsPay() {
+            return 0.15;
+        }
+    },
+    SILVER{
+        @Override
+        public double beneffitsPay() {
+            return 0.1;
+        }
+        },
+    BRONZE{
+    	@Override
+    	public double beneffitsPay() {
+    		return 0.05 ; 
+    	}
+    
+    }
+    ;
+
+    public abstract double beneffitsPay();
+}
+
+
