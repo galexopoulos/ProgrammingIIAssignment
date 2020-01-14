61b5fe586d2f827c24321f1cda12b6de79898d0e
Nikolas <Nikolas@DESKTOP-CG1EUKH>
2020-01-14 17:53:21 +0200

    fixed

--- src/src/test/java/ComputeCheckTest.java
+++ src/src/test/java/ComputeCheckTest.java
@@ -3,29 +3,65 @@ package src.test.java;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Calendar;
 import java.util.Date;
 
 import org.junit.jupiter.api.Test;
+
+import src.main.java.gr.aueb.dmst.erp.Booking;
+import src.main.java.gr.aueb.dmst.erp.Room;
 
 class ComputeCheckTest {
 
 	@Test
 	void test1() {
-		src.main.java.gr.aueb.dmst.erp.Room r = new src.main.java.gr.aueb.dmst.erp.Room(1, 2, 50);
-		Date checkIn1 = new Date();
-		Date checkOut1 = new Date();
-		src.main.java.gr.aueb.dmst.erp.Booking b1 = new src.main.java.gr.aueb.dmst.erp.Booking(checkIn1, checkOut1, 1, true);
+		Room r = new Room(1, 2, 50);
+		Calendar Cal = Calendar.getInstance();
+		Cal.set(Calendar.YEAR, 2020);
+		Cal.set(Calendar.MONTH, 2 - 1);
+		Cal.set(Calendar.DAY_OF_MONTH, 24);
+		Cal.set(Calendar.HOUR_OF_DAY, 12);
+		Cal.set(Calendar.MINUTE, 0);
+		Cal.set(Calendar.SECOND, 0);
+		Cal.set(Calendar.MILLISECOND, 0);
+		Date checkIn = Cal.getTime();
+		Cal = Calendar.getInstance();
+		Cal.set(Calendar.YEAR, 2020);
+		Cal.set(Calendar.MONTH, 2 - 1);
+		Cal.set(Calendar.DAY_OF_MONTH, 25);
+		Cal.set(Calendar.HOUR_OF_DAY, 11);
+		Cal.set(Calendar.MINUTE, 0);
+		Cal.set(Calendar.SECOND, 0);
+		Cal.set(Calendar.MILLISECOND, 0);
+		Date checkOut = Cal.getTime();
+		Booking b1 = new Booking(checkIn, checkOut, 1, true);
 		double output1 = b1.computeCheck(true);
 		assertEquals(62, output1);
 	}
 
 	@Test
 	void test2() {
-		src.main.java.gr.aueb.dmst.erp.Room r = new src.main.java.gr.aueb.dmst.erp.Room(1, 2, 50);
-		Date checkIn2 = new Date();
-		Date checkOut2 = new Date();
-		src.main.java.gr.aueb.dmst.erp.Booking b2 = new src.main.java.gr.aueb.dmst.erp.Booking(checkIn2, checkOut2, 1, false);
+		Room r = new Room(1, 2, 50);
+		Calendar Cal = Calendar.getInstance();
+		Cal.set(Calendar.YEAR, 2020);
+		Cal.set(Calendar.MONTH, 2 - 1);
+		Cal.set(Calendar.DAY_OF_MONTH, 26);
+		Cal.set(Calendar.HOUR_OF_DAY, 12);
+		Cal.set(Calendar.MINUTE, 0);
+		Cal.set(Calendar.SECOND, 0);
+		Cal.set(Calendar.MILLISECOND, 0);
+		Date checkIn = Cal.getTime();
+		Cal = Calendar.getInstance();
+		Cal.set(Calendar.YEAR, 2020);
+		Cal.set(Calendar.MONTH, 2 - 1);
+		Cal.set(Calendar.DAY_OF_MONTH, 28);
+		Cal.set(Calendar.HOUR_OF_DAY, 11);
+		Cal.set(Calendar.MINUTE, 0);
+		Cal.set(Calendar.SECOND, 0);
+		Cal.set(Calendar.MILLISECOND, 0);
+		Date checkOut = Cal.getTime();
+		Booking b2 = new Booking(checkIn, checkOut, 2, false);
 		double output2 = b2.computeCheck(false);
-		assertEquals(50, output2);
+		assertEquals(100, output2);
 	}
 }
