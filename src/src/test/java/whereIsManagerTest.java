493d37cb0041f6310e97345a9ffb3860411ba087
giorgossideris <56915448+giorgossideris@users.noreply.github.com>
2020-01-14 18:43:49 +0200

    change the test in order to run in maven too

--- src/src/test/java/whereIsManagerTest.java
+++ src/src/test/java/whereIsManagerTest.java
@@ -3,15 +3,24 @@ package src.test.java;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+
 import org.junit.jupiter.api.Test;
+import src.main.java.gr.aueb.dmst.erp.*;
+
 
 class whereIsManagerTest {
 
 	@Test
 	void test() {
+		//src.main.java.gr.aueb.dmst.erp.Employee.Employees.clear();
 		new src.main.java.gr.aueb.dmst.erp.Manager("Ivan", "Zaytsev", "Athlete", "Petreasismydaddy", 12000, null);
-		int output = src.main.java.gr.aueb.dmst.erp.Manager.whereIsManager(0);
-		assertEquals(0, output);
+		new src.main.java.gr.aueb.dmst.erp.Employee("Ivan", "Zaytsev", "Athlete", "Petreasismydaddy", 12000, null);
+		new src.main.java.gr.aueb.dmst.erp.Employee("Ivan", "Zaytsev", "Athlete", "Petreasismydaddy", 12000, null);
+		new src.main.java.gr.aueb.dmst.erp.Employee("Ivan", "Zaytsev", "Athlete", "Petreasismydaddy", 12000, null);
+		new src.main.java.gr.aueb.dmst.erp.Manager("Ivan", "Zaytsev", "Athlete", "Petreasismydaddy", 12000, null);
+		int output = src.main.java.gr.aueb.dmst.erp.Manager.whereIsManager(4);
+		assertEquals(4, output);
 	}
 
 }
+
